171632c89ed15e1d8cf8d4aced2bae735ed60c9a
Balaji Laygude <balajiwork05@gmail.com>
2026-09-11 16:36:30 +0530

    Changeing data formats

--- backend/src/validators/user.validator.js
+++ backend/src/validators/user.validator.js
@@ -32,17 +32,12 @@ const createUserValidation = [
 ];
 
 const createStoreValidation = [
-  body("name")
-    .trim()
-    .isLength({
-      min: 20,
-      max: 60,
-    })
-    .withMessage(
-      "Store name must be between 20 and 60 characters"
-    ),
 
-  
+
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("Please provide a valid email"),
 
   body("address")
     .trim()
